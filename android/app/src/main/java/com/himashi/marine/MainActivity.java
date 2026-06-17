f78727e6fc7367cff82256adc45b68f8a83b089e
Chathuranga-srk <bhanukachathuranga777@gmail.com>
2026-06-17 15:53:34 +0530

    First Native Build

---
+++ android/app/src/main/java/com/himashi/marine/MainActivity.java
@@ -0,0 +1,5 @@
+package com.himashi.marine;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/Himashi_App.bat
@@ -0,0 +1,3 @@
+@echo off
+start msedge --app="https://himashi-marine-lashing-management.netlify.app"
+exit
